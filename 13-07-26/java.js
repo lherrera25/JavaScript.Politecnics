672546298b06882a7e04ea744ef1327374a84be7
Lily <lherrera25@politecnics.barcelona>
2026-07-13 17:02:49 +0200

    subir

---
+++ 13-07-26/java.js
@@ -0,0 +1,33 @@
+function fecha(){
+  console.log(new Date());
+}
+fecha();
+
+function saludar(nombre){
+  console.log(`Hola ${nombre}`)
+
+}
+saludar('lily')
+
+function sumaInutil(){
+  let a =2
+  let b =3
+  document.write('esta funcion escribe un' +(a+b))
+}
+sumaInutil()
+
+function suma(a,b){
+  console.log('esta suma da' + a+b)
+ }
+ suma(4,8);
+  function sumaTop(c,d){
+    return c+d
+   
+  }
+  
+  let salida = sumaTop(20,7)
+  document.write(salida);
+
+  let fecha = Date.now(
+    console.log(aa)
+  )  
