502a6d02454f7085e4d1f8ceccc782d57d988775
Lily <lherrera25@politecnics.barcelona>
2026-07-30 17:59:55 +0200

    a

---
+++ 30-07-26/scripts.js
@@ -0,0 +1,37 @@
+let numeroDecimal = document.getElementById('numeroDecimal')
+const btnProbarRedondeo = document.getElementById('btnProbarRedondeo')
+let resultadoRound = document.getElementById('resultadoRound')
+let resultadoCeil = document.getElementById('resultadoCeil')
+let resultadoFloor = document.getElementById('resultadoFloor')
+
+let numeroAleatorio = document.getElementById('numeroAleatorio')
+const btnGenerarDecimal = document.getElementById('btnGenerarDecimal')
+
+
+let carasDados = document.getElementById('carasDado')
+let caraDados = document.getElementById('caraDado')
+const btnLanzarDado = document.getElementById('btnLanzarDado')
+
+
+
+
+//Listeners
+
+btnProbarRedondeo.addEventListener('click', ()=>{
+    const numero = Number(numeroDecimal.value)
+
+    resultadoRound.textContent = Math.round(numero)
+    resultadoCeil.textContent = Math.ceil(numero)
+    resultadoFloor.textContent = Math.floor(numero)
+})
+
+btnGenerarDecimal.addEventListener('click', ()=>{
+    const numero = Math.random()
+    numeroAleatorio.textContent = numero.toFixed(4)
+})
+
+
+//Funciones
+function lanzarDado(numeroCaras = 6){
+    return Math.random(Marth.random()*numeroCaras)+1
+}
